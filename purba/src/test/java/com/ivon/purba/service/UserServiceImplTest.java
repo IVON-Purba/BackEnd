4e918588dfe0ac78e5951045685964a1ddaa3fd3
ym2629 <dydals2629@naver.com>
2024-03-27 17:14:09 +0900

    test: sign up with invalid phone number pattern

--- purba/src/test/java/com/ivon/purba/service/UserServiceImplTest.java
+++ purba/src/test/java/com/ivon/purba/service/UserServiceImplTest.java
@@ -1,6 +1,7 @@
 package com.ivon.purba.service;
 
 import com.ivon.purba.dto.userController.SignUpRequest;
+import com.ivon.purba.exception.InvalidPhoneNumberPatternException;
 import com.ivon.purba.exception.UserAlreadyExistException;
 import com.ivon.purba.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -27,7 +28,6 @@ class UserServiceImplTest {
 
         //when
         userService.signUp(signUpRequest);
-
         UserAlreadyExistException e = assertThrows(UserAlreadyExistException.class, () -> {
             userService.signUp(signUpRequest);
         });
@@ -36,9 +36,26 @@ class UserServiceImplTest {
         assertThat(e.getMessage()).isEqualTo("이미 존재하는 유저입니다.");
     }
 
+    @Test
+    @DisplayName("유효하지 않은 전화번호로 회원가입")
+    void signUpWithInvalidPhoneNumberPatter(){
+        //given
+        SignUpRequest signUpRequest = new SignUpRequest();
+        signUpRequest.setPhoneNumber("My nave is Yongmin Kim");
+
+        //when
+        InvalidPhoneNumberPatternException e = assertThrows(InvalidPhoneNumberPatternException.class, () -> {
+            userService.signUp(signUpRequest);
+        });
+
+        //then
+        assertThat(e.getMessage()).isEqualTo("폰 번호 형식이 올바르지 않습니다.");
+    }
+
+
     private String generateRandomPhoneNumber() {
-        int middle = ThreadLocalRandom.current().nextInt(10000, 1000000000);
-        int last = ThreadLocalRandom.current().nextInt(10000, 1000000000);
+        int middle = ThreadLocalRandom.current().nextInt(1000, 10000);
+        int last = ThreadLocalRandom.current().nextInt(1000, 10000);
         return String.format("010-%04d-%04d", middle, last);
     }
 }
